8e3df2790e2aa5357dabc956da042a7136a918b3
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-07-31 12:46:55 +0300

    done

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,14 +4,17 @@ import {useState, useEffect} from 'react';
 export default function Home() {
 
   const [message, setMessage] = useState("Loading");
+  const [people, setPeople] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:8080/api/home").then(
       response => response.json()
     ).then(
       data => {
-        console.log(data)
+        console.log(data);
         setMessage(data.message);
+        setPeople(data.people);
+
       }
     )
   }, []);
@@ -19,6 +22,13 @@ export default function Home() {
   return (
     <main>
       <div>{message}</div>
+      <div>
+        {people.map((person, index) => (
+          <div key={index}>
+            {person}
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
